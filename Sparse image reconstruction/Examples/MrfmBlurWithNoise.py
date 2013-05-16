Small fixes. - Created MrfmBlurExample.py to just plot the MRFM psf - Fixed static method MrfmBlur.GetXyzMeshFor2d to get desired number of pts in mesh - Fixed calculation of blur shift induced by the MRFM psf

Sparse image reconstruction/Examples/MrfmBlurWithNoise.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import pylab as plt
 
@@ -12,8 +13,9 @@ class MrfmBlurWithNoise(AbstractExample):
     """
     Simulates 2d MRFM blur and optionally adds AWGN.
     """
-    def __init__(self, simParametersDict):
+    def __init__(self, optimizer, simParametersDict):
         super(MrfmBlurWithNoise, self).__init__('MrfmBlur with additive Gaussian noise example')
+        self._optimizer = optimizer
         self._simParametersDict = simParametersDict
         self.blurredImageWithNoise = None
         self.channelChain = None
@@ -37,33 +39,43 @@ class MrfmBlurWithNoise(AbstractExample):
         try:
             imageShape = self._simParametersDict[AbstractImageGenerator.INPUT_KEY_IMAGE_SHAPE]
         except KeyError:
-            imageShape = (32, 32)
+            imageShape = (42, 42)
 
         noiseSigma = self.NoiseSigma            
         snrDb = self.SnrDb
 
         # Use numpy.mgrid to generate 3-d grid mesh          
-        xyzMesh = MrfmBlur.GetXyzMeshFor2d(self._simParametersDict['xspan'], 
-                                           self._simParametersDict['z0'], 
-                                           np.min([16, np.floor(imageShape[0]/2)])
+        xyzMesh = MrfmBlur.GetXyzMeshFor2d(self._optimizer.xSpan, 
+                                           self._optimizer.z0, 
+                                           np.min([32, imageShape[0]])
                                            )
-        self._simParametersDict[MrfmBlur.INPUT_KEY_XMESH] = np.array(xyzMesh[0], dtype=float)
-        self._simParametersDict[MrfmBlur.INPUT_KEY_YMESH] = np.array(xyzMesh[1], dtype=float)
-        self._simParametersDict[MrfmBlur.INPUT_KEY_ZMESH] = np.array(xyzMesh[2], dtype=float)
                     
         # Create the MRFM blur      
-        mb = MrfmBlur(MrfmBlur.BLUR_2D, self._simParametersDict)
+        mb = MrfmBlur(MrfmBlur.BLUR_2D, 
+                      {
+                       MrfmBlur.INPUT_KEY_BEXT: self._optimizer.Bext,
+                       MrfmBlur.INPUT_KEY_BRES: self._optimizer.Bres,
+                       MrfmBlur.INPUT_KEY_SMALL_M: self._optimizer.m,
+                       MrfmBlur.INPUT_KEY_XPK: self._optimizer.xPk,
+                       MrfmBlur.INPUT_KEY_XMESH: np.array(xyzMesh[1], dtype=float),                                                 
+                       MrfmBlur.INPUT_KEY_YMESH: np.array(xyzMesh[0], dtype=float),
+                       MrfmBlur.INPUT_KEY_ZMESH: np.array(xyzMesh[2], dtype=float)
+                       }
+                      )
+        mb._GetBlurPsf()
     
         # Construct the processing chain
         channelChain = ChannelProcessingChain(True)
         
         ig = ImageGeneratorFactory.GetImageGenerator('random_binary_2d')
+        igBorderWidth = int(math.ceil((max(mb.PsfSupport[0]) - min(mb.PsfSupport[0]))/2.0)) + 1; # 1 shouldn't be necessary really
         ig.SetParameters(**{ 
                             AbstractImageGenerator.INPUT_KEY_IMAGE_SHAPE: imageShape,
                             AbstractImageGenerator.INPUT_KEY_NUM_NONZERO: numNonzero,
-                            AbstractImageGenerator.INPUT_KEY_BORDER_WIDTH: max(mb.BlurShift)
+                            AbstractImageGenerator.INPUT_KEY_BORDER_WIDTH: igBorderWidth
                            }
                          )
+        print("Border width in image generator is {0}".format(igBorderWidth))
         
         ng = NoiseGeneratorFactory.GetNoiseGenerator('additive_gaussian')
         if (noiseSigma is not None) and (noiseSigma >= 0):
@@ -98,16 +110,30 @@ class MrfmBlurWithNoise(AbstractExample):
         self.blurPsfInThetaFrame = mb.BlurPsfInThetaFrame      
                 
 if __name__ == "__main__":   
-    opti = MrfmBlurParameterOptimizer()
-    opti.CalcOptimalValues(3e4, 3)   
+    # Generate MRFM psf used in 04/f/sp_img_recon.m (less realistic parameters than those used in psf_sim_sing.m)
+    opti = MrfmBlurParameterOptimizer(deltaB0=100)
+    opti.bUseSmallerR0 = True
+    opti.bUseSmallerB0 = False
+    opti.CalcOptimalValues(1e4, 6, R0=4)    
+    
     # Construct the example object
-    ex = MrfmBlurWithNoise({
-                            'snrdb': 20,
-                            MrfmBlur.INPUT_KEY_BEXT: opti.Bext,
-                            MrfmBlur.INPUT_KEY_BRES: opti.Bres,
-                            MrfmBlur.INPUT_KEY_SMALL_M: opti.m,
-                            MrfmBlur.INPUT_KEY_XPK: opti.xPk,
-                            'xspan': opti.xSpan,
-                            'z0': opti.z0
-                            })
-    ex.RunExample()              
+    ex = MrfmBlurWithNoise(opti, 
+                           { 
+                            AbstractAdditiveNoiseGenerator.INPUT_KEY_SNRDB: 20 
+                            }
+                           )
+    ex.RunExample()
+       
+    # In order to remove the shift, must access the SyntheticBlur block in the channel chain
+    print("Blur shift is: " + str(ex.channelChain.channelBlocks[1].BlurShift))
+    blurredImageWithNoiseForDisplay = ex.channelChain \
+                                        .channelBlocks[1] \
+                                        .RemoveShiftFromBlurredImage(ex.blurredImageWithNoise)
+    plt.figure(1)
+    plt.imshow(ex.channelChain.intermediateOutput[0], interpolation='none')
+    plt.figure()
+    plt.imshow(ex.blurPsfInThetaFrame, interpolation='none')
+    plt.figure()
+    plt.imshow(blurredImageWithNoiseForDisplay, interpolation='none')
+    plt.colorbar()
+    plt.show()               

Sparse image reconstruction/Sim/Blur.py
@@ -1,5 +1,6 @@
 import abc
 import Channel.ChannelBlock as chb
+import math
 import numpy as np
 
 class AbstractBlur(chb.AbstractChannelBlock):
@@ -18,10 +19,11 @@ class AbstractBlur(chb.AbstractChannelBlock):
             raise UnboundLocalError("BlurShift isn't defined")
         else:
             assert len(anImage.shape) == len(self.BlurShift)
-            tmp = np.array(anImage)            
+#            tmp = np.array(anImage)
+            tmp = anImage            
             assert np.all(self.BlurShift > 0)            
             for axisInd in range(len(self.BlurShift)):                
-                tmp = np.roll(tmp, -self.BlurShift[axisInd], axisInd)
+                tmp = np.roll(tmp, int(-math.floor(self.BlurShift[axisInd])), axis=axisInd)
             return np.matrix(tmp)
 
     @property

Sparse image reconstruction/Sim/MrfmBlur.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import pylab as plt
 from Blur import AbstractBlur
 
 class MrfmBlur(AbstractBlur):
@@ -8,8 +9,8 @@ class MrfmBlur(AbstractBlur):
     """   
     @staticmethod
     def GetXyzMeshFor2d(xSpan, z0, numPoints):
-        meshDelta = 2*xSpan/numPoints
-        xyzMesh = np.mgrid[-xSpan:(xSpan + meshDelta):meshDelta, -xSpan:(xSpan + meshDelta):meshDelta, z0:(z0 + .1):.1]
+        meshDelta = 2.0 * xSpan / (numPoints - 1.0)
+        xyzMesh = np.mgrid[-xSpan:(xSpan + 0.1*meshDelta):meshDelta, -xSpan:(xSpan + 0.1*meshDelta):meshDelta, z0:(z0 + .09):.1]
         return xyzMesh
              
     @staticmethod
@@ -128,6 +129,7 @@ class MrfmBlur(AbstractBlur):
         # Initialize
         self._blurPsf = None
         self._blurShift = None
+        self._psfSupport = None
         
         if (blurType == MrfmBlur.BLUR_2D):
             if ((self.X is not None) and (self.Y is not None) and (self.Z is not None)):
@@ -150,6 +152,10 @@ class MrfmBlur(AbstractBlur):
     def Z(self):
         return self._zMesh
         
+    @property
+    def PsfSupport(self):
+        return self._psfSupport
+            
     def _GetBlurPsf(self):
         if ((self.X is None) or (self.Y is None) or (self.Z is None)):
             raise UnboundLocalError('Cannot get psf since mesh definition is undefined')
@@ -158,9 +164,12 @@ class MrfmBlur(AbstractBlur):
             psf2d = psf[:, :, 0]                
             self._blurPsf = psf2d            
             # Find support of psf in the x and y plane             
-            psfSupport = np.where(psf2d > self._eps)
+            psfSupport = np.where(psf2d > 2*self._eps)
+            self._psfSupport = psfSupport
             # The blur shift is half of the max support in both x and y
-            self._blurShift = (math.floor(max(psfSupport[0]) / 2), math.floor(max(psfSupport[1]) / 2))         
+            blurShiftInXDir = min(psfSupport[0]) + math.floor((max(psfSupport[0]) - min(psfSupport[0])) / 2)
+            blurShiftInYDir = min(psfSupport[1]) + math.floor((max(psfSupport[1]) - min(psfSupport[1])) / 2)            
+            self._blurShift = (blurShiftInXDir, blurShiftInYDir)       
         
     def BlurImage(self, theta):       
         if (self._blurType == MrfmBlur.BLUR_2D):
@@ -171,7 +180,9 @@ class MrfmBlur(AbstractBlur):
             if (self._blurPsf is None):
                 self._GetBlurPsf()
             if (not np.all(self._thetaShape >= self._blurPsf.shape)):
-                raise ValueError("theta's shape must be at least as big as the blur's shape")            
+                raise ValueError("theta's shape: " + str(self._thetaShape) + 
+                                 " must be at least as big as the blur's shape: " + str(self._blurPsf.shape)
+                                 )            
                                                            
             y = np.fft.ifft2(np.multiply(np.fft.fft2(self.BlurPsfInThetaFrame), np.fft.fft2(theta)))
             return y.real                        